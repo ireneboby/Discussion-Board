005cfd85c4acb9ba99aaaba196ed4194b62ef471
Irene Boby <ireneboby.dev@gmail.com>
2023-08-30 11:19:56 -0400

    Added create new rooms and show room actions

---
+++ routes/rooms.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('./helpers/auth');
+const Room = require('../models/room');
+
+// Rooms index
+router.get('/', (req, res, next) => {
+    Room.find({}, 'topic').
+    then((rooms) => res.render('rooms/index', { rooms: rooms }))
+    .catch ((err) => console.error(err));
+});
+
+// Rooms new
+router.get('/new', auth.requireLogin, (req, res, next) => {
+    res.render('rooms/new');
+  });
+
+// Rooms show
+router.get('/:id', auth.requireLogin, (req, res, next) => {
+  Room.findById(req.params.id)
+  .then((room) => res.render('rooms/show', { room: room }))
+  .catch ((err) => console.error(err));
+});
+
+// Rooms edit
+router.get('/:id/edit', auth.requireLogin, (req, res, next) => {
+  // TODO
+});
+
+// Rooms update
+router.post('/:id', auth.requireLogin, (req, res, next) => {
+  // TODO
+});
+
+// Rooms create
+router.post('/', auth.requireLogin, (req, res, next) => {
+    const room = new Room(req.body);
+    room.save().then(()=> res.redirect('/rooms')).catch((err) => console.log(err));
+});
+
+module.exports = router;
